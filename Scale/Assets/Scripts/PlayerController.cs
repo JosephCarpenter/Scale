6ea61b1880aed05b7050758734b592d70c05bef0
Joe Carpenter <carp5@rcn.com>
2022-03-29 18:16:19 -0400

    Resize Player

--- Scale/Assets/Scripts/PlayerController.cs
+++ Scale/Assets/Scripts/PlayerController.cs
@@ -1,76 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// 
-// // Include the namespace required to use Unity UI and Input System
-// using UnityEngine.InputSystem;
-// using TMPro;
-// 
-// public class PlayerController : MonoBehaviour {
-// 
-// 	// Create public variables for player speed, and for the Text UI game objects
-// 	public float speed;
-// 	// public TextMeshProUGUI countText;
-// 	// public GameObject winTextObject;
-// 
-//     private float movementX;
-//     private float movementY;
-// 
-// 	private Rigidbody rb;
-// 	private int count;
-// 
-// 	// At the start of the game..
-// 	void Start () {
-// 		// Assign the Rigidbody component to our private rb variable
-// 		rb = GetComponent<Rigidbody>();
-// 
-// 		// Set the count to zero 
-// 		count = 0;
-// 
-// 		// SetCountText();
-//         // // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
-//         // winTextObject.SetActive(false);
-// 	}
-// 
-// 	void FixedUpdate () {
-// 		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
-// 		Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-// 
-// 		rb.AddForce(movement * speed);
-// 	}
-// 
-// 	void OnTriggerEnter(Collider other) {
-// 		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
-// 		if (other.gameObject.CompareTag ("Pickup"))
-// 		{
-// 			other.gameObject.SetActive (false);
-// 
-// 			// Add one to the score variable 'count'
-// 			count = count + 1;
-// 
-// 			// Run the 'SetCountText()' function (see below)
-// 			// SetCountText ();
-// 		}
-// 	}
-// 
-//     void OnMove(InputValue value) {
-//     	Vector2 v = value.Get<Vector2>();
-// 
-//     	movementX = v.x;
-//     	movementY = v.y;
-//     }
-// 
-//     // void SetCountText() {
-// 	// 	countText.text = "Count: " + count.ToString();
-//     // 
-// 	// 	if (count >= 9) 
-// 	// 	{
-//     //         // Set the text value of your 'winText'
-//     //         winTextObject.SetActive(true);
-// 	// 	}
-// 	// }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -83,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed;
     public float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    public float playerSize = 1.0f;
 
     private void Start()
     {
@@ -91,6 +19,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        updateScale();
+        
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -113,5 +43,15 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+    
+    void updateScale() {
+        if (Input.GetMouseButtonDown(0)) {
+            playerSize += 0.1f;
+        } else if (Input.GetMouseButtonDown(1)) {
+            playerSize -= 0.1f;
+        }        
+        
+        gameObject.transform.localScale = new Vector3(playerSize, playerSize, playerSize); 
     }
 }
