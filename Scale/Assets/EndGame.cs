89bcf84bf9096eb87d2fac9061a252b0106b2092
JEendahl01 <jendahl01@gmail.com>
2022-05-06 16:35:49 -0400

    Added Idle animation

--- Scale/Assets/EndGame.cs
+++ Scale/Assets/EndGame.cs
@@ -7,19 +7,19 @@ public class EndGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool raiseDoor = true;
-        foreach (DoorTrigger plate in plates) {
-            if (!plate.isOpened)
-                raiseDoor = false;
-        }
-        if (raiseDoor) {
-            // ENDGAME
-        }
+        // bool raiseDoor = true;
+        // foreach (DoorTrigger plate in plates) {
+        //     if (!plate.isOpened)
+        //         raiseDoor = false;
+        // }
+        // if (raiseDoor) {
+        //     // ENDGAME
+        // }
     }
 }
